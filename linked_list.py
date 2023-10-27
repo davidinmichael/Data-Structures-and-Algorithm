b8bcd02dbdc221c16a0264ac9e18f3d00c59ce6b
davidinmichael <davidinmichael@gmail.com>
2023-10-27 04:09:42 +0100

    I added the insert method for LinkedList

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,6 @@
+from turtle import position
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -54,8 +57,9 @@ class LinkedList:
         temp = self.head
         self.head = self.head.next
         temp.next = None
+        self.length -= 1
         return temp
-    
+
     def pop_last(self):
         if self.length == 0:
             return None
@@ -64,7 +68,42 @@ class LinkedList:
             pre = temp
             temp = temp.next
         pre.next = None
+        self.length -= 1
         return temp
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        elif index == 0:
+            return self.pop_first()
+        elif index == self.length - 1:
+            return self.pop_last()
+        else:
+            prev = self.get_item(index - 1)
+            temp = prev.next
+            prev.next = temp.next
+            temp.next = None
+            self.length -= 1
+            return temp
+
+    def set_item(self, index, value):
+        item = self.get_item(index)
+        item.value = value
+        return item
+
+    def insert(self, index, value):
+        new_node = Node(value)
+        if index < 0 or index > self.length:
+            return False
+        elif index == 0:
+            return self.prepend(value)
+        elif index == self.length:
+            return self.prepend(value)
+        temp = self.get_item(index - 1)
+        next = temp.next
+        temp.next = new_node
+        new_node.next = next
+        return new_node
 
 
 my_llist = LinkedList()
@@ -73,9 +112,15 @@ my_llist.append(6)
 my_llist.append(7)
 my_llist.append(8)
 my_llist.prepend(4)
+# my_llist.get_list()
+# my_llist.remove(2)
+# my_llist.get_list()
+# my_llist.pop_first()
+# my_llist.get_list()
+# my_llist.pop_last()
+# my_llist.get_list()
+# my_llist.set_item(0, 1)
 my_llist.get_list()
-my_llist.pop_first()
-my_llist.get_list()
-my_llist.pop_last()
+my_llist.insert(4, 3)
 my_llist.get_list()
 # my_llist.get_item(3)
