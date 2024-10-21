331aa690a40ef66e652da90702fdef8d0366b338
davidinmichael <davidinmichael@gmail.com>
2024-10-21 14:38:41 +0100

    Print list and append method added for DLL.

--- 02_doubly_linkedlist.py
+++ 02_doubly_linkedlist.py
@@ -3,3 +3,31 @@ class Node:
         self.value = value
         self.next = None
         self.prev = None
+    
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+    
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+    
+    def append(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        return True
+
+
+my_doubly_linkedlist = DoublyLinkedList(1)
+my_doubly_linkedlist.print_list()
