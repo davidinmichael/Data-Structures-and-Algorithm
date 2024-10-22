ce35e8f702a7a3a8d6eefd35efe19b0e3890b0d1
davidinmichael <davidinmichael@gmail.com>
2024-10-22 14:36:25 +0100

    Constructor and print list for Queue added

--- 03_stack.py
+++ 03_stack.py
@@ -37,10 +37,11 @@ class Stack:
             self.top = self.top.next
             temp.next = None
             self.height -= 1
-            return True
+            return temp
 
 
 my_stack = Stack(1)
 my_stack.push(2)
+my_stack.push(3)
 my_stack.pop()
 my_stack.print_stack()
